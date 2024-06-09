64f5fd17de3714f12b8ee3586a3136e9952de909
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-06-09 07:14:51 -0300

    feat: it was created ocoren entity mapper

---
+++ src/main/java/com/github/setxpro/tms_ocurrences/infra/mappers/OcorenMapper.java
@@ -0,0 +1,47 @@
+package com.github.setxpro.tms_ocurrences.infra.mappers;
+
+import com.github.setxpro.tms_ocurrences.domain.core.entities.Ocoren;
+import com.github.setxpro.tms_ocurrences.infra.database.models.OcorenEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OcorenMapper {
+    public OcorenEntity toEntity(Ocoren ocoren) {
+        return new OcorenEntity(
+                ocoren.getId(),
+                ocoren.getNumeroNFe(),
+                ocoren.getNumeroCTe(),
+                ocoren.getSerie(),
+                ocoren.getData(),
+                ocoren.getHora(),
+                ocoren.getCnpjTransportadora(),
+                ocoren.getNomeRecebedor(),
+                ocoren.getCnpjPagador(),
+                ocoren.getCodigo(),
+                ocoren.getOcorrencia(),
+                ocoren.getTransportadora(),
+                ocoren.getUfDestino(),
+                ocoren.getCidadeDestino(),
+                ocoren.getDescricao()
+        );
+    }
+    public Ocoren toEntity(OcorenEntity ocoren) {
+        return new Ocoren(
+                ocoren.getId(),
+                ocoren.getNumeroNFe(),
+                ocoren.getNumeroCTe(),
+                ocoren.getSerie(),
+                ocoren.getData(),
+                ocoren.getHora(),
+                ocoren.getCnpjTransportadora(),
+                ocoren.getNomeRecebedor(),
+                ocoren.getCnpjPagador(),
+                ocoren.getCodigo(),
+                ocoren.getOcorrencia(),
+                ocoren.getTransportadora(),
+                ocoren.getUfDestino(),
+                ocoren.getCidadeDestino(),
+                ocoren.getDescricao()
+        );
+    }
+}
